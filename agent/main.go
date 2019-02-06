0dde99a7afccb305934933ee9aa5a6297fa957b2
Paul Burakov <pburakov@gmail.com>
2019-02-06 21:05:34 +0000

    Add debug log

--- agent/main.go
+++ agent/main.go
@@ -65,6 +65,7 @@ func getExternalIP() string {
 }
 
 func setUpConnection(address string) *grpc.ClientConn {
+	log.Printf("Connecting to %s", address)
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
